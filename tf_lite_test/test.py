0398eae3c0c85b64a3aa54fdb93a73f3b94aae20
Nabeel Nasir <nabeeln7@gmail.com>
2018-11-24 23:23:48 -0500

    initial commit

---
+++ tf_lite_test/test.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow import keras
+from tensorflow.contrib import lite
+
+model = keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
+model.compile(optimizer='sgd', loss='mean_squared_error')
+
+xs = np.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)
+ys = np.array([-3.0, -1.0, 1.0, 3.0, 5.0, 7.0], dtype=float)
+
+model.fit(xs, ys, epochs=500)
+
+print model.predict([10.0])
+
+# write the Keras save file
+keras_file = "linear.h5"
+keras.models.save_model(model, keras_file)
+
+# convert the Keras file to tensorflow lite file
+converter = lite.TFLiteConverter.from_keras_model_file(keras_file)
+tflite_model = converter.convert()
+open("linear.tflite", "wb").write(tflite_model)
+
+
